d6af41893d36a1f3882631a820ecc714a3bebde4
Victor Casta <victorarroyavecastaneda@gmail.com>
2023-10-19 12:59:00 -0500

    [ADD] inserts new page and new component for map section

---
+++ src/components/Map.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<div class="Map">
+    <div class="container__Map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63462.74754690655!2d-75.63174388870293!3d6.207973999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442973cab4d10f%3A0x6137cd2f289c6af8!2sCentro%20de%20Salud%20SURA!5e0!3m2!1ses-419!2sco!4v1697734645055!5m2!1ses-419!2sco" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="mapa sura google maps"></iframe>
+    </div>
+</div>
+
+<style>
+    .Map {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding: 30px 0;
+    }
+    .Map .container__Map {
+        width: 90%;
+    }
+</style>
